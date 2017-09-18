0810f5db8e9ee254b6bb5ce880b1a4af31b1b947
yandersantana <ysantanatavares.1996@hotmail.com>
2017-09-17 23:43:35 -0500

    agregando algo

--- server.js
+++ server.js
@@ -42,6 +42,49 @@ app.use(parser.urlencoded({
 
 
 
+
+app.post('/subir', (req, res) => {
+    req.fields; // contains non-file fields 
+    req.files; // contains files 
+
+
+    var form = new formidable.IncomingForm();
+
+    // parse a file upload
+    form.parse(req, function (err, fields, files) {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+
+        //res.end(util.inspect({fields: fields, files: files}));
+
+
+    });
+    form.on('end', function (fields, files) {
+        /* Temporary location of our uploaded file */
+        var temp_path = this.openedFiles[0].path;
+        /* The file name of the uploaded file */
+        var file_name = this.openedFiles[0].name;
+        console.log(file_name);
+        /* Location where we want to copy the uploaded file */
+        var new_location = 'public/imagenes/historias/';
+        fs.copy(temp_path, new_location + file_name, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("success!")
+            }
+        });
+        res.end(file_name);
+    });
+
+});
+
+
+
+
+
+
 app.get('/cargar', (req, res, next) => {
     const client = new pg.Client(conString);
     client.connect(function (err) {
